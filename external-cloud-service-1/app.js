6527b0eba844c37c7d911dee53155acfc665b74b
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2025-05-03 16:53:54 +0700

    fix route path

--- external-cloud-service-1/app.js
+++ external-cloud-service-1/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/api/external', (req, res) => {
+app.get('/api/external-data', (req, res) => {
   res.json({ message: 'You accessed the EXTERNAL CLOUD SERVICE (RBAC success).' });
 });
 
--- external-cloud-service-2/app.js
+++ external-cloud-service-2/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/api/secure-report', (req, res) => {
+app.get('/api/external-report', (req, res) => {
   res.json({ message: 'You accessed the SECURE REPORT in Cloud Service 2 (RBAC success).' });
 });
 
