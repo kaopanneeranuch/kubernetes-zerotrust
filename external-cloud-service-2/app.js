751684a38a580fa961396d3a6474a8f342336bb8
Neeranuch Jitkhajornwanich <jitkhajornwanich.n@gmail.com>
2025-05-06 01:57:05 +0700

    fix port conflict

--- external-cloud-service-2/app.js
+++ external-cloud-service-2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/external-report', (req, res) => {
   res.json({ message: 'You accessed the SECURE REPORT in Cloud Service 2.' });
